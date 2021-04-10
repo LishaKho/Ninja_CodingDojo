62f0ce1b577717f39b39ebab0c9d6c8785915ba5
LishaKho <lkoesnod@yahoo.com>
2021-04-10 16:05:04 -0700

    First Commit

---
+++ ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(`My Name is ${this.name}`);
+    }
+    showStats() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+    }
+    drinkSake() {
+        console.log(`Adding Health: ${this.health += 10}`);
+    }
+}
+
+const ninja1 = new Ninja("Hayabusa", 100)
+ninja1.sayName()
+ninja1.showStats()
+ninja1.drinkSake()
